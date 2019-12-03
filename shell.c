a78b91d5b8138bf3e71148f9be8043adfdd2ee81
mmiah00 <mmiah00@stuy.edu>
2019-12-03 11:00:55 -0500

    writing execute

--- shell.c
+++ shell.c
@@ -3,3 +3,24 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include "shell.h"
+
+char ** parse_args (char * line) {
+  char ** ans = malloc (256);
+  int i = 0;
+  char * now = line;
+  while (now) {
+    ans [i] = strsep (&now, " ");
+    i ++;
+  }
+
+  return ans;
+}
+
+void execute (char* command) {
+  char * line = strdup (command);
+  char ** args = parse_args (line);
+  printf ("%s...\n", command);
+  execvp (args[0], args);
+  printf ("Didn't work"); 
+}
