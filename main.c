6d49c731e2b0bb2070f1ca417bf7016d8ae00f32
Brandon <bchen00@stuy.edu>
2019-12-05 10:50:31 -0500

    wrote a bit of main

--- main.c
+++ main.c
@@ -3,7 +3,11 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include "shell.h"
 
 int main ()  {
+  char line1[100] = "ls -1";
+  char *p = "ls -1";//&line1;
+  executeOne(p);
   return 0;
 }
---
+++ shell.h
@@ -0,0 +1,3 @@
+char ** parse_args (char * line);
+void executeOne (char* command);
+void execmultiple (char* command);
