ec4f6f0dfa908191e67fb48b0c74af90b9fa7a57
mmiah00 <mmiah00@stuy.edu>
2019-12-08 18:08:41 +0000

    added header

--- shell.h
+++ shell.h
@@ -1,4 +1,5 @@
 char ** parse_args (char * line);
 char ** parse_argsSemi (char * line);
 void executeOne (char** args);
+void redirect (char * file1, char * file2); 
 //void execmultiple (char* command);
