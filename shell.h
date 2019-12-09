181e6d10d216070f737aab877e0f9fc309f2b90d
mmiah00 <mmiah00@stuy.edu>
2019-12-09 00:07:40 +0000

    deleted part

--- shell.h
+++ shell.h
@@ -1,5 +1,5 @@
 char ** parse_args (char * line);
 char ** parse_argsSemi (char * line);
 void executeOne (char** args);
-void redirect (char * file1, char * file2); 
+//void redirect (char * line); 
 //void execmultiple (char* command);
