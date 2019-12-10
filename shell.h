0ddb5c6800e236ac32945a22aa4c39003b3268f9
mmiah00 <mmiah00@stuy.edu>
2019-12-10 11:00:16 -0500

    header

--- shell.h
+++ shell.h
@@ -1,5 +1,11 @@
 char ** parse_args (char * line);
 char ** parse_argsSemi (char * line);
+
+
 void executeOne (char** args);
-//void redirect (char * line); 
+
+void redirect (char * line);
+
+
+//void redirect (char * line);
 //void execmultiple (char* command);
